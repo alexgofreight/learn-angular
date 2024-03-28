b1a2fba24fac8b5fe146bb8f8a47e798ed9a3058
Alex <alex.wang@gofreight.com>
2024-03-28 11:58:05 +0800

    add 3-party lib and fix ts issues

--- Hello/src/app/app.component.ts
+++ Hello/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Hello';
+export class AppComponent implements OnInit{
+  title = 'Hello'
+  fruits = ['apple', 'orange', 'banana']
+  myModal: any
+
+  ngOnInit(): void {
+    this.myModal = new bootstrap.Modal('#exampleModal', {
+      keyboard: false
+    })
+  }
+
+  openModal(): void {
+    this.myModal.show()
+  }
 }
