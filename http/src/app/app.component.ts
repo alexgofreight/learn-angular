35047fb13d8ea7f2aec14b1a4e08afb4655f2975
Alex <alex.wang@gofreight.com>
2024-03-31 16:44:46 +0800

    add first example

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './@models/todo.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'http';
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get<Todo[]>('assets/todo.json').subscribe(data=>{
+      console.log(data)
+    })
+  }
 }
