7972814b308a6ddf73a33640b947ebc031489f06
Alex <alex.wang@gofreight.com>
2024-04-02 01:30:05 +0800

    add formGroup

--- react-form/src/app/sign-up/sign-up.component.ts
+++ react-form/src/app/sign-up/sign-up.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
-  username = new FormControl("");
+export class SignUpComponent implements OnInit{
+  
+  
+  userForm: FormGroup;
+
+  constructor() {
+    this.userForm = new FormGroup({
+      username: new FormControl(""),
+      password: new FormControl(""),
+      confirmPassword: new FormControl("")
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
 
   clear() {
-    this.username.reset();
-    // this.username.setValue('');
+    this.userForm.reset();
+  }
+
+  onSubmit() {
+    console.log(this.userForm.value)
   }
 }
